394db79eff1ba1f700ac3f00038594d40606c25e
Kirill <akirilltikhonov@gmail.com>
2020-08-06 23:22:26 +0300

    .

--- TestTaskDexsys/src/TestTask.java
+++ TestTaskDexsys/src/TestTask.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,6 +13,7 @@ public class TestTask {
 
 //    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 //    24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+//    0 -1 -3 -7 -14 -18 -21
     public static void main(String[] args) throws IOException {
 
         init(args);
@@ -19,46 +21,48 @@ public class TestTask {
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String str = null;
+
             while (!(str = reader.readLine()).equals("exit")) {
-                switch (str) {
-                    case "print":
-                        printAll();
-                        break;
-                    case "print X":
-                        print(list3, "X");
-                        break;
-                    case "print S":
-                        print(list7, "S");
-                        break;
-                    case "print M":
-                        print(list21, "M");
-                        break;
-                    case "clear X":
-                        list3.clear();
-                        break;
-                    case "clear S":
-                        list7.clear();
-                        break;
-                    case "clear M":
-                        list21.clear();
-                        break;
-                    case "merge":
-                        merge();
-                        break;
-                    default :
-                        System.out.println("Incorrect command. Call 'help' and try again");
-                        break;
+
+                if (str.matches("init(\\s[-+]?\\d+)+\\s*")) {
+                    String [] array = str.substring(5).split(" ");
+                    init(array);
+                } else {
+                    switch (str) {
+                        case "print":
+                            printAll();
+                            break;
+                        case "print X":
+                            print(list3, ListType.M);
+                            break;
+                        case "print S":
+                            print(list7, ListType.S);
+                            break;
+                        case "print M":
+                            print(list21, ListType.M);
+                            break;
+                        case "clear X":
+                            list3.clear();
+                            break;
+                        case "clear S":
+                            list7.clear();
+                            break;
+                        case "clear M":
+                            list21.clear();
+                            break;
+                        case "merge":
+                            merge();
+                            break;
+                        case "help":
+                            help();
+                        default:
+                            System.out.println("Incorrect command. Call 'help' and try again");
+                            break;
+                    }
                 }
             }
             System.out.println("Goodbye");
         }
-    }
-
-    public static void merge() {
-        System.out.println(Stream.of(list3, list7, list21).flatMap(x -> x.stream()).collect(Collectors.toList()));
-        list3.clear();
-        list7.clear();
-        list21.clear();
     }
 
     public static void init(String[] array) {
@@ -84,18 +88,74 @@ public class TestTask {
         Collections.sort(list7);
         Collections.sort(list21);
     }
+
     static public void printAll() {
-        print(list3, "X");
-        print(list7, "S");
-        print(list21, "M");
+        print(list3, ListType.X);
+        print(list7, ListType.S);
+        print(list21, ListType.M);
     }
-    static public void print(List<Integer> list, String type) {
+
+    static public void print(List<Integer> list, ListType type) {
         if (list.size() == 0) {
             System.out.println("Список " + type + " пуст");
         } else {
             System.out.println(String.format("Список %s : %s", type, list));
         }
-
     }
 
+    public static void merge() {
+        System.out.println(Stream.of(list3, list7, list21).flatMap(x -> x.stream()).collect(Collectors.toList()));
+        list3.clear();
+        list7.clear();
+        list21.clear();
+    }
+
+    public static void help() {
+        Files.readAllLines()
+    }
 }
+
+
+
+//if (str.matches("init [\\d\\s]+")) {
+//        String [] array = str.substring(5).split(" ");
+////                    Arrays.stream(array).forEach(x -> System.out.print(x));
+//        init(array);
+//
+//        } else if (str.matches("print(\\s[XSM])?")) {
+//        switch (str) {
+//        case "print":
+//        printAll();
+//        break;
+//        case "print X":
+//        print(list3, "X");
+//        break;
+//        case "print S":
+//        print(list7, "S");
+//        break;
+//        case "print M":
+//        print(list21, "M");
+//        break;
+//        }
+//        } else if (str.matches("clear [XSM]|merge")) {
+//        switch (str) {
+//        case "clear X":
+//        list3.clear();
+//        break;
+//        case "clear S":
+//        list7.clear();
+//        break;
+//        case "clear M":
+//        list21.clear();
+//        break;
+//        case "merge":
+//        merge();
+//        break;
+//        }
+//        }
+////                else if (str.equals("merge")) {
+////                    merge();
+////                }
+//        else {
+//        System.out.println("Incorrect command. Call 'help' and try again");
+//        }
